73f26eabdd540c055033c9998d1d96c509d14e39
minty1202 <aki.men1202@gmail.com>
2022-02-23 15:07:27 +0900

    [add] htmlの作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,48 @@ import React from 'react';
 import './style.css';
 
 export const App = () => {
-  return 'Hello World';
+  return (
+    <>
+      <div>
+        <input />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+          <ul>
+            <li>
+              <div>
+                <p>リスト</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>リスト</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+          <ul>
+            <li>
+              <div>
+                <p>リスト</p>
+                <button>戻す</button>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>リスト</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          </ul>
+      </div>
+    </>
+  );
 }
