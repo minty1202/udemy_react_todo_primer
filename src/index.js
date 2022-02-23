0bf89a50f26faf06a2788ebb0780e4614e898c9e
minty1202 <aki.men1202@gmail.com>
2022-02-23 15:00:04 +0900

    [fix] 基礎準備

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDom from 'react-dom';
 
-const App = () => {
-  return 'hello world'
-};
+import { App } from './App'
 
 ReactDom.render(<App />, document.getElementById("root"));
