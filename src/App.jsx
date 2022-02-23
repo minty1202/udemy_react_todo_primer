696b151bed2706e7d9df6873ad36a8319a2c6cf3
minty1202 <aki.men1202@gmail.com>
2022-02-23 17:33:10 +0900

    [update] 投稿機能と削除機能の追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,41 @@ import React, { useState } from 'react';
 import './style.css';
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('')
   const [incompleteTodos, setIncompleteTodos] = useState(['リスト１', 'リスト２']);
   const [completeTodos, setCompleteTodos] = useState(['リスト３', 'リスト４']);
+  
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === '') return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText('');
+  }
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className='input-area'>
-        <input  placeholder="TODOを入力"/>
-        <button>追加</button>
+        <input  placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
           <ul>
-            {incompleteTodos.map((todo) => {
+            {incompleteTodos.map((todo, index) => {
               return (
                 <li key={todo}>
                   <div className='list-row'>
                     <p>{todo}</p>
                     <button>完了</button>
-                    <button>削除</button>
+                    <button onClick={() => onClickDelete(index)}>削除</button>
                   </div>
                 </li>
               );
